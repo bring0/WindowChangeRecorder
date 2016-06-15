c2b785beb50e3102714e477c5c6a84dffb5abf49
Brendan Ring <bring0@icloud.com>
2016-06-15 00:04:02 -0500

    Created Tray Application, reworked solution structure

---
+++ Everything/TrayApplication/ProcessIcon.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using TrayApplication.Properties;
+using System.Drawing;
+
+namespace TrayApplication
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class ProcessIcon : IDisposable
+    {
+        /// <summary>
+        /// The NotifyIcon object.
+        /// </summary>
+        NotifyIcon ni;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProcessIcon"/> class.
+        /// </summary>
+        public ProcessIcon()
+        {
+            // Instantiate the NotifyIcon object.
+            ni = new NotifyIcon();
+        }
+
+        /// <summary>
+        /// Displays the icon in the system tray.
+        /// </summary>
+        public void Display()
+        {
+            // Put the icon in the system tray and allow it react to mouse clicks.			
+            ni.MouseClick += new MouseEventHandler(ni_MouseClick);
+            ni.Icon = Resources.TrayIcon;
+            ni.Text = "System Tray Utility Application Demonstration Program";
+            ni.Visible = true;
+
+            // Attach a context menu.
+            ni.ContextMenuStrip = new ContextMenus().Create();
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        public void Dispose()
+        {
+            // When the application closes, this will remove the icon from the system tray immediately.
+            ni.Dispose();
+        }
+
+        /// <summary>
+        /// Handles the MouseClick event of the ni control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.Forms.MouseEventArgs"/> instance containing the event data.</param>
+        void ni_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Handle mouse button clicks.
+            if (e.Button == MouseButtons.Left)
+            {
+                // Start Windows Explorer.
+                Process.Start("explorer", null);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class ContextMenus
+    {
+        /// <summary>
+        /// Is the About box displayed?
+        /// </summary>
+        bool isAboutLoaded = false;
+
+        /// <summary>
+        /// Creates this instance.
+        /// </summary>
+        /// <returns>ContextMenuStrip</returns>
+        public ContextMenuStrip Create()
+        {
+            // Add the default menu options.
+            ContextMenuStrip menu = new ContextMenuStrip();
+            ToolStripMenuItem item;
+            ToolStripSeparator sep;
+
+            // Windows Explorer.
+            item = new ToolStripMenuItem();
+            item.Text = "Disable";
+            item.Click += new EventHandler(Disable_Click);
+            item.Image = Resources.Disable;
+            menu.Items.Add(item);
+
+            // About.
+            item = new ToolStripMenuItem();
+            item.Text = "About";
+            item.Click += new EventHandler(About_Click);
+            item.Image = Resources.About;
+            menu.Items.Add(item);
+
+            // Separator.
+            sep = new ToolStripSeparator();
+            menu.Items.Add(sep);
+
+            // Exit.
+            item = new ToolStripMenuItem();
+            item.Text = "Exit";
+            item.Click += new System.EventHandler(Exit_Click);
+            item.Image = Resources.Exit;
+            menu.Items.Add(item);
+
+            return menu;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the Explorer control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        void Disable_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer", null);
+        }
+
+        /// <summary>
+        /// Handles the Click event of the About control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        void About_Click(object sender, EventArgs e)
+        {
+            if (!isAboutLoaded)
+            {
+                isAboutLoaded = true;
+                new AboutBox().ShowDialog();
+                isAboutLoaded = false;
+            }
+        }
+
+        /// <summary>
+        /// Processes a menu item.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        void Exit_Click(object sender, EventArgs e)
+        {
+            // Quit without further ado.
+            Application.Exit();
+        }
+    }
+}
